d8f76fc6d4588bf391ea7a60d3380f33192d27a1
aimanow <40722311+aimanow@users.noreply.github.com>
2019-12-23 11:45:02 +0100

    descAdd.js
    Changed translation

--- src/front/src/lang/de/descAdd.js
+++ src/front/src/lang/de/descAdd.js
@@ -9,14 +9,14 @@ module.exports = {
   select: 'Aspekte auswählen',
   selectOne: 'Aspekte auswählen',
   arg: 'Argument',
-  theses: 'Thesis', //*
+  theses: 'These', //*
   links: 'Referenzen:',
   publish: 'Thema verfassen',
   yes: 'Ja',
   no: 'Nein',
   aspects: 'Meine Aspekte',
-  searchAspects: 'Choose Aspect',//*
-  nameAspects: 'Name of Aspects', //*
+  searchAspects: 'Aspekte auswählen',//*
+  nameAspects: 'Aspektname', //*
   attachments: 'Anhänge',
   addImage: 'Bild hinzufügen',
   addNewAspect: 'Fügen Sie Ihre eigene Aspektoption hinzu',
@@ -35,8 +35,8 @@ module.exports = {
 
   errorMinThesisArgument: 'Bitte überprüfen Sie die Felder These und Argumente die Felder müssen mindestens 10 Zeichen lang sein',
   errorMinThesis: 'Bitte überprüfen Sie die Felder These die Felder müssen mindestens 10 Zeichen lang sein',
-  errorMaxArgument: 'Bitte überprüfen Sie, ob das Argumentfeld kürzer als 100 Zeichen sein muss',
-  errorMaxThesis: 'Bitte überprüfen Sie, ob das Nachrichtenfeld für die Abschlussarbeit weniger als 1000 Zeichen lang sein muss',
-  errorChooseAspects: 'Bitte überprüfen Sie die Aspekte, es können nur maximal drei Aspekte ausgewählt werden',
+  errorMaxArgument: 'Argumentfeld muss kürzer als 100 Zeichen sein',
+  errorMaxThesis: 'Nachrichtenfeld für die These kürzer als 1000 Zeichen lang sein',
+  errorChooseAspects: 'Bitte überprüfen Sie die Aspekte, es können maximal drei Aspekte ausgewählt werden',
   errorYesNo: 'Bitte Ja oder Nein ankreuzen',
 }
